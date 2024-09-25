78c3329ef6964c90e1403575d4605d85d3e90096
Ayumi Kumano <kumayuzuki@gmail.com>
2024-09-25 18:39:24 +0900

    Add markdown parse

---
+++ service-site/app/_components/TopCards.tsx
@@ -0,0 +1,39 @@
+import { Post } from "@/.contentlayer/generated";
+import Link from "next/link";
+import styles from "./TopCards.module.css";
+interface TopCardsProps {
+  posts: Post[];
+}
+
+export function TopCards({ posts }: TopCardsProps) {
+  return (
+    <div className={styles.topCards}>
+      {posts.map((post) => (
+        <Link href={post.href} key={post.href}>
+          <div className={styles.topCard}>
+            {post.image && (
+              <div className={styles.imageContainer}>
+                <img
+                  src={post.image}
+                  alt={`Image of ${post.title}`}
+                  width={300}
+                  height={200}
+                  className={styles.image}
+                />
+              </div>
+            )}
+            <h2 className={styles.title}>
+              {post.title}
+            </h2>
+            <p className={styles.writer}>
+              Text by {post.writer}
+            </p>
+            <div className={styles.introduction}>
+              {post.introduction}
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
