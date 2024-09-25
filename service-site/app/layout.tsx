22588b9360172ecf3593e7b504c3ba32fdd5063d
Ayumi Kumano <kumayuzuki@gmail.com>
2024-09-25 19:40:19 +0900

    Update tokens from Figma

--- service-site/app/layout.tsx
+++ service-site/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./styles/globals.css";
 import { Header, Footer } from "./_components";
 
 export const metadata: Metadata = {
