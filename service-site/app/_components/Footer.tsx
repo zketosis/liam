a79679f490fa4a522d04e05899310a90c84eeda3
Ayumi Kumano <kumayuzuki@gmail.com>
2024-09-25 19:11:18 +0900

    Draft Styling for TopCards

--- service-site/app/_components/Footer.tsx
+++ service-site/app/_components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = ({ className }: { className?: string }) => {
   return (
     <footer className={`${className} flex border-zinc-200 border-t p-4 dark:border-zinc-600`}>
       <p className="text-xs text-zinc-500 leading-5 dark:text-zinc-400">
-        &copy; 2024 XXX, All rights reserved.
+        &copy; 2024 Liam
       </p>
     </footer>
   );
