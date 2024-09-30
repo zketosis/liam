806e24bed4cd94929b29acf693077737b5af1335
Ayumi Kumano <kumayuzuki@gmail.com>
2024-09-30 14:04:42 +0900

    Add Liam logo image

--- service-site/app/_components/Header.tsx
+++ service-site/app/_components/Header.tsx
@@ -1,13 +1,22 @@
-
 import Link from 'next/link'
+import Image from "next/image";
 import styles from './Header.module.css';
 
 export const Header = () => {
     return (
       <header className={styles.header}>
-        <h1 className={styles.h1}>
-        <Link href="/">LOGO</Link>
-        </h1>
+        <Link href="/">
+          <h1 className={styles.h1}>
+            <Image
+              src="./images/logo.svg"
+              alt="Liam"
+              width={98}
+              height={30}
+              className={styles.image}
+              quality={100}
+            />
+          </h1>
+        </Link>
       </header>
     );
   };
