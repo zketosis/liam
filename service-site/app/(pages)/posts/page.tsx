715f60435eb9a19856aa3c79366d8f96bdc81dd6
Ayumi Kumano <kumayuzuki@gmail.com>
2024-10-01 15:42:30 +0900

    Remove tailwind css class names

--- service-site/app/(pages)/posts/page.tsx
+++ service-site/app/(pages)/posts/page.tsx
@@ -4,16 +4,16 @@ import { allPosts, Post } from 'contentlayer/generated'
 
 function PostCard(post: Post) {
   return (
-    <div className="mb-8">
-      <h2 className="mb-1 text-xl">
-        <Link href={post.href} className="text-blue-700 hover:text-blue-900 dark:text-blue-400">
+    <div>
+      <h2>
+        <Link href={post.href}>
           {post.title}
         </Link>
       </h2>
-      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
+      <time dateTime={post.date}>
         {format(parseISO(post.date), 'LLLL d, yyyy')}
       </time>
-      <div className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0" dangerouslySetInnerHTML={{ __html: post.body.html }} />
+      <div dangerouslySetInnerHTML={{ __html: post.body.html }} />
     </div>
   )
 }
@@ -22,8 +22,8 @@ export default function Home() {
   const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
 
   return (
-    <div className="mx-auto max-w-xl py-8">
-      <h1 className="mb-8 text-center text-2xl font-black">Posts</h1>
+    <div>
+      <h1>Posts</h1>
       {posts.map((post, idx) => (
         <PostCard key={idx} {...post} />
       ))}
