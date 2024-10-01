4a6e8a98be2efac583a7f99450ff31e37a18caf0
Ayumi Kumano <kumayuzuki@gmail.com>
2024-10-01 17:44:43 +0900

    Delete next.config.mjs

--- service-site/next.config.mjs
+++
@@ -1,6 +0,0 @@
-import { withContentlayer } from 'next-contentlayer';
-
-/** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, images: { domains: ['localhost'] } };
-
-export default withContentlayer(nextConfig)
