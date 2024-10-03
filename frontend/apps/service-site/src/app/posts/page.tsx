e85d37c479a5753c122f85e63554fdaf27d9715c
Junki Saito <jonki.beam@gmail.com>
2024-10-03 18:08:58 +0900

    Update frontend/apps/service-site/src/app/posts/page.tsx
    Co-authored-by: Hirotaka Miyagi <31152321+MH4GF@users.noreply.github.com>

--- frontend/apps/service-site/src/app/posts/page.tsx
+++ frontend/apps/service-site/src/app/posts/page.tsx
@@ -11,7 +11,7 @@ function PostCard(post: Post) {
       <time dateTime={post.date}>
         {format(parseISO(post.date), 'LLLL d, yyyy')}
       </time>
-      {/* biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation> */}
+      {/* biome-ignore lint/security/noDangerouslySetInnerHtml: No problem, as it is only via Markdown written by in-house members. */}
       <div dangerouslySetInnerHTML={{ __html: post.body.html }} />
     </div>
   )
