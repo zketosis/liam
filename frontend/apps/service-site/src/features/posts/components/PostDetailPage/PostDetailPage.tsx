a8f73cb609029b676d02e3f75bb08a5f1bae23e7
junkisai <jonki.beam@gmail.com>
2024-10-08 19:00:07 +0900

    refactor: Create PostDetailPage common component to eliminate code duplication

---
+++ frontend/apps/service-site/src/features/posts/components/PostDetailPage/PostDetailPage.tsx
@@ -0,0 +1,28 @@
+import type { Lang } from '@/features/i18n'
+import { MDXContent } from '@packages/mdx-components'
+import { format, parseISO } from 'date-fns'
+import { notFound } from 'next/navigation'
+import type { FC } from 'react'
+import { findPostByLangAndSlug } from '../../utils'
+
+type Props = {
+  lang: Lang
+  slug: string
+}
+
+export const PostDetailPage: FC<Props> = ({ lang, slug }) => {
+  const post = findPostByLangAndSlug({ lang, slug })
+  if (!post) notFound()
+
+  return (
+    <article>
+      <div>
+        <time dateTime={post.date}>
+          {format(parseISO(post.date), 'LLLL d, yyyy')}
+        </time>
+        <h1>{post.title}</h1>
+      </div>
+      <MDXContent code={post.body.code} />
+    </article>
+  )
+}
