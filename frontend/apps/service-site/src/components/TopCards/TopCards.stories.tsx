30ba848a40cb3fd82afc39a3b68d11d3e957c8af
Ayumi Kumano <kumayuzuki@gmail.com>
2024-10-08 11:55:23 +0900

    Add postId on TopCards.stories.tsx to make it easier to see posts order

--- frontend/apps/service-site/src/components/TopCards/TopCards.stories.tsx
+++ frontend/apps/service-site/src/components/TopCards/TopCards.stories.tsx
@@ -4,7 +4,7 @@ import { TopCards } from './'
 
 const commonPostData = (postId: number) => ({
   id: `${postId}`,
-  title: 'The No-Code Revolution: A New Era Where Non-Programmers Can Build Apps',
+  title: `${postId} The No-Code Revolution: A New Era Where Non-Programmers Can Build Apps`,
   tags: ['Democratization', 'Empowerment', 'Innovation', 'Agility', 'Disruption'],
   writer: 'Aiden Sparks',
   introduction: "The rise of no-code application platforms is changing the landscape of software development. These platforms allow users with little to no coding knowledge to create fully functional applications, reducing the barrier to entry for innovation. The convenience, flexibility, and speed offered by no-code tools are helping businesses of all sizes innovate at unprecedented rates.",
