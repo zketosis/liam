afc4937eb31690f55b0ebbe05b9671b99f48123b
Ayumi Kumano <kumayuzuki@gmail.com>
2024-10-08 11:48:01 +0900

    Add default story for TopCards

--- frontend/apps/service-site/src/components/TopCards/TopCards.stories.tsx
+++ frontend/apps/service-site/src/components/TopCards/TopCards.stories.tsx
@@ -2,10 +2,22 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { TopCards } from './'
 
+const commonPostData = (postId: number) => ({
+  id: `${postId}`,
+  title: 'The No-Code Revolution: A New Era Where Non-Programmers Can Build Apps',
+  tags: ['Democratization', 'Empowerment', 'Innovation', 'Agility', 'Disruption'],
+  writer: 'Aiden Sparks',
+  introduction: "The rise of no-code application platforms is changing the landscape of software development. These platforms allow users with little to no coding knowledge to create fully functional applications, reducing the barrier to entry for innovation. The convenience, flexibility, and speed offered by no-code tools are helping businesses of all sizes innovate at unprecedented rates.",
+  image: `/images/posts/${postId}/image.png`,
+});
+
 const meta = {
   component: TopCards,
   args: {
-    posts: [],
+    posts: Array.from({ length: 14 }, (_, i) => ({
+      ...commonPostData(i + 1),
+      _id: (i + 1).toString(),
+    })),
   },
 } satisfies Meta<typeof TopCards>
 
