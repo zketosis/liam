6f2a5077a90306896a2ca5b5a6364e9ece9a0796
MH4GF <h.miyagi.cnw@gmail.com>
2024-10-09 18:06:07 +0900

    feat: Add sample DropdownMenu component to @packages/ui

---
+++ frontend/packages/ui/src/components/DropdownMenu/DropdownMenu.tsx
@@ -0,0 +1,10 @@
+import type { FC } from 'react'
+import styles from './DropdownMenu.module.css'
+
+export const DropdownMenu: FC = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1>DropdownMenu Component</h1>
+    </div>
+  )
+}
