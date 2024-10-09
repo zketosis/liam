a275cb83fb728160c5ab3c30117ab4140de88a45
junkisai <jonki.beam@gmail.com>
2024-10-09 17:39:19 +0900

    feat: Implement conversion of Markdown headings and code to styled components

--- frontend/apps/service-site/src/libs/contentlayer/MDXContent.tsx
+++ frontend/apps/service-site/src/libs/contentlayer/MDXContent.tsx
@@ -1,10 +1,16 @@
 import { Callout } from '@/contents/components'
+import { Code, Heading } from '@/features/posts'
 import type { MDXComponents } from 'mdx/types'
 // eslint-disable-next-line no-restricted-imports
 import { useMDXComponent } from 'next-contentlayer2/hooks'
 import type { FC } from 'react'
 
 const mdxComponents: MDXComponents = {
+  h2: ({ children }) => <Heading as="h2">{children}</Heading>,
+  h3: ({ children }) => <Heading as="h3">{children}</Heading>,
+  h4: ({ children }) => <Heading as="h4">{children}</Heading>,
+  h5: ({ children }) => <Heading as="h5">{children}</Heading>,
+  code: ({ children }) => <Code>{children}</Code>,
   Callout: (props) => <Callout {...props} />,
 }
 
