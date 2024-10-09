cfe4bf5b07826c02d678a2af2a74cbb655b29e9a
Ayumi Kumano <kumayuzuki@gmail.com>
2024-10-09 19:48:44 +0900

    fmt

--- frontend/apps/service-site/src/components/ShareIconButton/ShareIconButton.d.ts
+++ frontend/apps/service-site/src/components/ShareIconButton/ShareIconButton.d.ts
@@ -1,10 +1,9 @@
 declare const styles: {
-  readonly "footer": string;
-  readonly "linksContainer": string;
-  readonly "logoContainer": string;
-  readonly "snsLink": string;
-  readonly "text": string;
-  readonly "textLink": string;
-};
-export = styles;
-
+  readonly footer: string
+  readonly linksContainer: string
+  readonly logoContainer: string
+  readonly snsLink: string
+  readonly text: string
+  readonly textLink: string
+}
+export = styles
--- frontend/apps/service-site/src/components/ShareIconButton/ShareIconButton.tsx
+++ frontend/apps/service-site/src/components/ShareIconButton/ShareIconButton.tsx
@@ -1,14 +1,12 @@
 import clsx from 'clsx'
-import styles from './ShareIconButton.module.css'
 import { Share } from '../Icons'
+import styles from './ShareIconButton.module.css'
 
 export const ShareIconButton = () => {
   return (
-    <button
-      className={clsx(styles.button)}
-    >
-      <Share className={clsx(styles.icon)}/>
+    <div className={clsx(styles.button)}>
+      <Share className={clsx(styles.icon)} />
       {/* Add tooltip component after import packages/ui/src/components */}
-    </button>
+    </div>
   )
 }
