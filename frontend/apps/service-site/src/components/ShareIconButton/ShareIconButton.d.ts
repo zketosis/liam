c88bfa83374f0db40f3514fa6cd17893358b073e
Ayumi Kumano <kumayuzuki@gmail.com>
2024-10-11 14:28:32 +0900

    refactor: remove unused file

--- frontend/apps/service-site/src/components/ShareIconButton/ShareIconButton.d.ts
+++
@@ -1,9 +0,0 @@
-declare const styles: {
-  readonly footer: string
-  readonly linksContainer: string
-  readonly logoContainer: string
-  readonly snsLink: string
-  readonly text: string
-  readonly textLink: string
-}
-export = styles
