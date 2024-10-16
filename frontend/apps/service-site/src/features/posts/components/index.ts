334e291139bcf42d32d3c9bc7f6c475b4605f21c
Ayumi Kumano <kumayuzuki@gmail.com>
2024-10-16 19:54:35 +0900

    feat: implement for BodyText component

--- frontend/apps/service-site/src/features/posts/components/index.ts
+++ frontend/apps/service-site/src/features/posts/components/index.ts
@@ -6,6 +6,7 @@ export * from './NavNextPost'
 export * from './NavPreviousPost'
 export * from './TableOfContents'
 export * from './PostHero'
+export * from './BodyText'
 
 export * from './PostDetailPage'
 export * from './PostListPage'
