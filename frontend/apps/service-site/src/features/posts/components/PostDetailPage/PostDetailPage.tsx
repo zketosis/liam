f1f5820179892618df35fb43a02be118402c3355
Ayumi Kumano <kumayuzuki@gmail.com>
2024-10-16 17:49:19 +0900

    refactor: fix conflicts

--- frontend/apps/service-site/src/features/posts/components/PostDetailPage/PostDetailPage.tsx
+++ frontend/apps/service-site/src/features/posts/components/PostDetailPage/PostDetailPage.tsx
@@ -1,5 +1,4 @@
 import { type Lang, fallbackLang } from '@/features/i18n'
-import { LinkHeading } from '@/features/posts/components/LinkHeading'
 import { PostCategories } from '@/features/posts/components/PostCategories'
 import { PostHero } from '@/features/posts/components/PostHero'
 import { MDXContent } from '@/libs/contentlayer'
@@ -29,33 +28,31 @@ export const PostDetailPage: FC<Props> = ({ lang, slug }) => {
     <article className={TOC_TARGET_CLASS_NAME}>
       <PostHero post={post} />
       <TableOfContents contentSelector={TOC_TARGET_CLASS_NAME} />
-      {/* FIXME: Add href props after implementing categories single page */}
-      <LinkHeading href="/">Categories</LinkHeading>
-      <MDXContent code={post.body.code} />
-      <div className={styles.navPostWrapper}>
-        {prevPost && (
-          <div className={styles.navPrev}>
-            <NavPreviousPost lang={lang} post={prevPost} />
-          </div>
-        )}
-        {nextPost && (
-          <div className={styles.navNext}>
-            <NavNextPost lang={lang} post={nextPost} />
-          </div>
-        )}
       <div className={styles.container}>
         <div className={styles.left}>
           <TableOfContents contentSelector={TOC_TARGET_CLASS_NAME} />
+          </div>
+          <div className={styles.center}>
+            <MDXContent code={post.body.code} />
+            <div className={styles.navPostWrapper}>
+              {prevPost && (
+                <div className={styles.navPrev}>
+                  <NavPreviousPost lang={lang} post={prevPost} />
+                </div>
+              )}
+              {nextPost && (
+          <div className={styles.navNext}>
+            <NavNextPost lang={lang} post={nextPost} />
+                </div>
+              )}
+            </div>
+          </div>
+          <div className={styles.right}>
+            <PostCategories
+              categories={post.categories.map((category) => ({ name: category }))}
+            />
+          </div>
         </div>
-        <div className={styles.center}>
-          <MDXContent code={post.body.code} />
-        </div>
-        <div className={styles.right}>
-          <PostCategories
-            categories={post.categories.map((category) => ({ name: category }))}
-          />
-        </div>
-      </div>
     </article>
   )
 }
