13fcba2ea2fe756e04a67bc95af5a437bf8fb138
junkisai <jonki.beam@gmail.com>
2024-10-16 16:03:33 +0900

    feat: Add NavXXXPost component to PostDetailPage

---
+++ frontend/apps/service-site/src/features/posts/utils/getPrevPost.ts
@@ -0,0 +1,24 @@
+import type { Lang } from '@/features/i18n'
+import type { Post } from 'contentlayer/generated'
+import { filterPostsByLang } from './filterPostsByLang'
+import { sortPostsByDate } from './sortPostsByDate'
+
+type Params = {
+  lang: Lang
+  targetPost: Post
+}
+
+export function getPrevPost({ lang, targetPost }: Params) {
+  const posts = filterPostsByLang(lang)
+  const sortedPosts = sortPostsByDate(posts)
+
+  const targetIndex = sortedPosts.findIndex(
+    (post) => post._id === targetPost._id,
+  )
+
+  if (targetIndex === -1 || targetIndex === 0) {
+    return undefined
+  }
+
+  return sortedPosts[targetIndex - 1]
+}
