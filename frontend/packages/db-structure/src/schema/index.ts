5c1dae1dfede48a2350b817642396a685e66d380
ryota.sasazawa <ryota.sasazawa@route06.co.jp>
2024-11-12 19:00:51 +0900

    add schema

--- frontend/packages/db-structure/src/schema/index.ts
+++ frontend/packages/db-structure/src/schema/index.ts
@@ -1,9 +1,51 @@
 import * as v from 'valibot'
 
-const tablesSchema = v.object({})
+const fieldSchema = v.object({
+  id: v.number(),
+  name: v.string(),
+  type: v.string(),
+  default: v.string(),
+  check: v.string(),
+  primary: v.boolean(),
+  unique: v.boolean(),
+  notNull: v.boolean(),
+  increment: v.boolean(),
+  comment: v.string(),
+})
+
+const indexSchema = v.object({
+  id: v.number(),
+  name: v.string(),
+  unique: v.boolean(),
+  fields: v.array(v.string()),
+})
+
+const tableSchema = v.object({
+  id: v.number(),
+  name: v.string(),
+  x: v.number(),
+  y: v.number(),
+  fields: v.array(fieldSchema),
+  comment: v.string(),
+  indices: v.array(indexSchema),
+  color: v.string(),
+})
+
+const relationshipSchema = v.object({
+  id: v.number(),
+  name: v.string(),
+  startTableId: v.number(),
+  startFieldId: v.number(),
+  endTableId: v.number(),
+  endFieldId: v.number(),
+  cardinality: v.string(),
+  updateConstraint: v.string(),
+  deleteConstraint: v.string(),
+})
 
 export const dbStructureSchema = v.object({
-  tables: tablesSchema,
+  tables: v.array(tableSchema),
+  relationships: v.array(relationshipSchema),
 })
 
 export type DBStructure = v.InferOutput<typeof dbStructureSchema>
