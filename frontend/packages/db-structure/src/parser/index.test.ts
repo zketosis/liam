1403bb263843b585c1ca9c8ce1900a1a7fbddf72
MH4GF <h.miyagi.cnw@gmail.com>
2024-11-18 14:29:30 +0900

    Refactor: change to snapshot testing

--- frontend/packages/db-structure/src/parser/index.test.ts
+++ frontend/packages/db-structure/src/parser/index.test.ts
@@ -9,12 +9,8 @@ describe('Schema Parser', () => {
       path.resolve(__dirname, './schemarb/input/schema1.in.rb'),
       'utf-8',
     )
-    const expectedJson = fs.readFileSync(
-      path.resolve(__dirname, './schemarb/output/schema1.out.json'),
-      'utf-8',
-    )
 
     const result = Parser.parse(schemaText, 'schemarb')
-    expect(result).toEqual(JSON.parse(expectedJson))
+    expect(result).toMatchSnapshot()
   })
 })
