55cdbd763923282582ec42305ce161920ef516d2
MH4GF <h.miyagi.cnw@gmail.com>
2024-11-22 16:54:45 +0900

    maintenance: Add small size tests for parsing create_table in schemarb

---
+++ frontend/packages/db-structure/src/parser/schemarb/index.test.ts
@@ -0,0 +1,67 @@
+import type { Table } from 'src/schema'
+import { aDBStructure, aField, aTable } from 'src/schema/factories'
+import { describe, expect, it } from 'vitest'
+import { processor } from '.'
+
+describe(processor, () => {
+  describe('should parse create_table correctry', () => {
+    const userTable = (override?: Partial<Table>) =>
+      aDBStructure({
+        tables: {
+          users: aTable({
+            name: 'users',
+            fields: [aField({ name: 'id' }), ...(override?.fields ?? [])],
+          }),
+        },
+      })
+
+    it('not null', () => {
+      const result = processor(`
+        create_table "users" do |t|
+          t.string "name", null: false
+        end
+      `)
+
+      const expected = userTable({
+        fields: [aField({ name: 'name', type: 'string', notNull: true })],
+      })
+
+      expect(result).toEqual(expected)
+    })
+
+    it('nullable', () => {
+      const result = processor(`
+        create_table "users" do |t|
+          t.string "name", null: true
+        end
+      `)
+
+      const expected = userTable({
+        fields: [aField({ name: 'name', type: 'string', notNull: false })],
+      })
+
+      expect(result).toEqual(expected)
+    })
+
+    it('defalt value', () => {
+      const result = processor(`
+        create_table "users" do |t|
+          t.string "name", default: "new user", null: true
+        end
+      `)
+
+      const expected = userTable({
+        fields: [
+          aField({
+            name: 'name',
+            type: 'string',
+            notNull: false,
+            default: 'new user',
+          }),
+        ],
+      })
+
+      expect(result).toEqual(expected)
+    })
+  })
+})
