3a4bf2f86554aeaf641bc534650d7fd92ae207e2
MH4GF <h.miyagi.cnw@gmail.com>
2024-11-22 19:34:34 +0900

    fix(convertDBStructureToNodes): change to dynamically generate nodes based on the tables in the DBStructure

--- frontend/packages/erd-core/src/components/ERDRenderer/convertDBStructureToNodes.ts
+++ frontend/packages/erd-core/src/components/ERDRenderer/convertDBStructureToNodes.ts
@@ -2,30 +2,13 @@ import type { DBStructure } from '@liam/db-structure'
 import type { Node } from '@xyflow/react'
 
 export const convertDBStructureToNodes = (dbStructure: DBStructure): Node[] => {
-  // TODO: Implement this function
-  const tableNames = Object.keys(dbStructure.tables)
-
-  return [
-    {
-      id: '1',
-      data: { label: tableNames[0] || 'A table node' },
-      position: { x: 0, y: 50 },
-    },
-    {
-      id: '2',
-      data: { label: tableNames[1] || 'A table node' },
-      style: { border: '1px solid #777', padding: 10 },
-      position: { x: 300, y: 50 },
-    },
-    {
-      id: '3',
-      data: { label: tableNames[2] || 'A table node' },
-      position: { x: 650, y: 25 },
-    },
-    {
-      id: '4',
-      data: { label: tableNames[3] || 'A table node' },
-      position: { x: 650, y: 100 },
-    },
-  ]
+  const tables = Object.values(dbStructure.tables)
+  return tables.map((table, index) => {
+    return {
+      id: table.name,
+      data: { label: table.name },
+      // TODO: layout the nodes
+      position: { x: index * 300, y: 50 },
+    }
+  })
 }
