d6109db79ef2e7900d5037ec2c5b506136a95bd9
MH4GF <h.miyagi.cnw@gmail.com>
2024-12-02 20:41:02 +0900

    feat: add Sidebar component and styles, update package dependencies

---
+++ frontend/packages/ui/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,436 @@
+'use client'
+
+import { Slot } from '@radix-ui/react-slot'
+import clsx from 'clsx'
+import { PanelLeft } from 'lucide-react'
+import {
+  type CSSProperties,
+  type ComponentProps,
+  type ElementRef,
+  createContext,
+  forwardRef,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
+import {
+  TooltipContent,
+  TooltipProvider,
+  TooltipRoot,
+  TooltipTrigger,
+} from '../Tooltip'
+import styles from './Sidebar.module.css'
+
+const SIDEBAR_COOKIE_NAME = 'sidebar:state'
+const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
+const SIDEBAR_WIDTH = '16rem'
+const SIDEBAR_WIDTH_ICON = '3rem'
+const SIDEBAR_KEYBOARD_SHORTCUT = 'b'
+
+type SidebarContext = {
+  state: 'expanded' | 'collapsed'
+  open: boolean
+  setOpen: (open: boolean) => void
+  toggleSidebar: () => void
+}
+
+const SidebarContext = createContext<SidebarContext | null>(null)
+
+function useSidebar() {
+  const context = useContext(SidebarContext)
+  if (!context) {
+    throw new Error('useSidebar must be used within a SidebarProvider.')
+  }
+
+  return context
+}
+
+const SidebarProvider = forwardRef<
+  HTMLDivElement,
+  ComponentProps<'div'> & {
+    defaultOpen?: boolean
+    open?: boolean
+    onOpenChange?: (open: boolean) => void
+  }
+>(
+  (
+    {
+      defaultOpen = true,
+      open: openProp,
+      onOpenChange: setOpenProp,
+      className,
+      style,
+      children,
+      ...props
+    },
+    ref,
+  ) => {
+    const [openMobile, setOpenMobile] = useState(false)
+
+    // This is the internal state of the sidebar.
+    // We use openProp and setOpenProp for control from outside the component.
+    const [_open, _setOpen] = useState(defaultOpen)
+    const open = openProp ?? _open
+    const setOpen = useCallback(
+      (value: boolean | ((value: boolean) => boolean)) => {
+        const openState = typeof value === 'function' ? value(open) : value
+        if (setOpenProp) {
+          setOpenProp(openState)
+        } else {
+          _setOpen(openState)
+        }
+
+        // This sets the cookie to keep the sidebar state.
+        document.cookie = `${SIDEBAR_COOKIE_NAME}=${openState}; path=/; max-age=${SIDEBAR_COOKIE_MAX_AGE}`
+      },
+      [setOpenProp, open],
+    )
+
+    // Helper to toggle the sidebar.
+    const toggleSidebar = useCallback(() => {
+      return setOpen((open) => !open)
+    }, [setOpen])
+
+    // Adds a keyboard shortcut to toggle the sidebar.
+    useEffect(() => {
+      const handleKeyDown = (event: KeyboardEvent) => {
+        if (
+          event.key === SIDEBAR_KEYBOARD_SHORTCUT &&
+          (event.metaKey || event.ctrlKey)
+        ) {
+          event.preventDefault()
+          toggleSidebar()
+        }
+      }
+
+      window.addEventListener('keydown', handleKeyDown)
+      return () => window.removeEventListener('keydown', handleKeyDown)
+    }, [toggleSidebar])
+
+    // We add a state so that we can do data-state="expanded" or "collapsed".
+    // This makes it easier to style the sidebar with Tailwind classes.
+    const state = open ? 'expanded' : 'collapsed'
+
+    const contextValue = useMemo<SidebarContext>(
+      () => ({
+        state,
+        open,
+        setOpen,
+        openMobile,
+        setOpenMobile,
+        toggleSidebar,
+      }),
+      [state, open, setOpen, openMobile, toggleSidebar],
+    )
+
+    return (
+      <SidebarContext.Provider value={contextValue}>
+        <TooltipProvider delayDuration={0}>
+          <div
+            style={
+              {
+                '--sidebar-width': SIDEBAR_WIDTH,
+                '--sidebar-width-icon': SIDEBAR_WIDTH_ICON,
+                ...style,
+              } as CSSProperties
+            }
+            className={clsx(styles.sidebarProvider, className)}
+            ref={ref}
+            {...props}
+          >
+            {children}
+          </div>
+        </TooltipProvider>
+      </SidebarContext.Provider>
+    )
+  },
+)
+SidebarProvider.displayName = 'SidebarProvider'
+
+const Sidebar = forwardRef<
+  HTMLDivElement,
+  ComponentProps<'div'> & {
+    collapsible?: 'offcanvas' | 'icon' | 'none'
+  }
+>(({ collapsible = 'offcanvas', className, children, ...props }, ref) => {
+  const { state } = useSidebar()
+
+  if (collapsible === 'none') {
+    return (
+      <div className={clsx(className)} ref={ref} {...props}>
+        {children}
+      </div>
+    )
+  }
+
+  return (
+    <div
+      ref={ref}
+      data-state={state}
+      data-collapsible={state === 'collapsed' ? collapsible : ''}
+    >
+      {/* This is what handles the sidebar gap on desktop */}
+      <div className={styles.sidebarGap} />
+      <div className={clsx(styles.sidebarWrapper, className)} {...props}>
+        <div data-sidebar="sidebar" className={styles.sidebar}>
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+})
+Sidebar.displayName = 'Sidebar'
+
+const SidebarTrigger = forwardRef<
+  ElementRef<'button'>,
+  ComponentProps<'button'>
+>(({ className, onClick, ...props }, ref) => {
+  const { toggleSidebar } = useSidebar()
+
+  return (
+    <button
+      ref={ref}
+      data-sidebar="trigger"
+      className={clsx(className)}
+      onClick={(event) => {
+        onClick?.(event)
+        toggleSidebar()
+      }}
+      {...props}
+    >
+      <PanelLeft />
+    </button>
+  )
+})
+SidebarTrigger.displayName = 'SidebarTrigger'
+
+const SidebarRail = forwardRef<HTMLButtonElement, ComponentProps<'button'>>(
+  ({ className, ...props }, ref) => {
+    const { toggleSidebar } = useSidebar()
+
+    return (
+      <button
+        ref={ref}
+        data-sidebar="rail"
+        aria-label="Toggle Sidebar"
+        tabIndex={-1}
+        onClick={toggleSidebar}
+        title="Toggle Sidebar"
+        className={clsx(styles.sidebarRail, className)}
+        {...props}
+      />
+    )
+  },
+)
+SidebarRail.displayName = 'SidebarRail'
+
+const SidebarHeader = forwardRef<HTMLDivElement, ComponentProps<'div'>>(
+  ({ className, ...props }, ref) => {
+    return <div ref={ref} data-sidebar="header" {...props} />
+  },
+)
+SidebarHeader.displayName = 'SidebarHeader'
+
+const SidebarFooter = forwardRef<HTMLDivElement, ComponentProps<'div'>>(
+  ({ className, ...props }, ref) => {
+    return <div ref={ref} data-sidebar="footer" {...props} />
+  },
+)
+SidebarFooter.displayName = 'SidebarFooter'
+
+const SidebarContent = forwardRef<HTMLDivElement, ComponentProps<'div'>>(
+  ({ className, ...props }, ref) => {
+    return <div ref={ref} data-sidebar="content" {...props} />
+  },
+)
+SidebarContent.displayName = 'SidebarContent'
+
+const SidebarGroup = forwardRef<HTMLDivElement, ComponentProps<'div'>>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        data-sidebar="group"
+        className={clsx(styles.sidebarGroup, className)}
+        {...props}
+      />
+    )
+  },
+)
+SidebarGroup.displayName = 'SidebarGroup'
+
+const SidebarGroupLabel = forwardRef<
+  HTMLDivElement,
+  ComponentProps<'div'> & { asChild?: boolean }
+>(({ className, asChild = false, ...props }, ref) => {
+  const Comp = asChild ? Slot : 'div'
+
+  return (
+    <Comp
+      ref={ref}
+      data-sidebar="group-label"
+      className={clsx(styles.sidebarGroupLabel, className)}
+      {...props}
+    />
+  )
+})
+SidebarGroupLabel.displayName = 'SidebarGroupLabel'
+
+const SidebarGroupAction = forwardRef<
+  HTMLButtonElement,
+  ComponentProps<'button'> & { asChild?: boolean }
+>(({ className, asChild = false, ...props }, ref) => {
+  const Comp = asChild ? Slot : 'button'
+
+  return <Comp ref={ref} data-sidebar="group-action" {...props} />
+})
+SidebarGroupAction.displayName = 'SidebarGroupAction'
+
+const SidebarGroupContent = forwardRef<HTMLDivElement, ComponentProps<'div'>>(
+  ({ className, ...props }, ref) => (
+    <div
+      ref={ref}
+      data-sidebar="group-content"
+      className={clsx(className)}
+      {...props}
+    />
+  ),
+)
+SidebarGroupContent.displayName = 'SidebarGroupContent'
+
+const SidebarMenu = forwardRef<HTMLUListElement, ComponentProps<'ul'>>(
+  ({ className, ...props }, ref) => (
+    <ul ref={ref} data-sidebar="menu" {...props} />
+  ),
+)
+SidebarMenu.displayName = 'SidebarMenu'
+
+const SidebarMenuItem = forwardRef<HTMLLIElement, ComponentProps<'li'>>(
+  ({ className, ...props }, ref) => (
+    <li ref={ref} data-sidebar="menu-item" {...props} />
+  ),
+)
+SidebarMenuItem.displayName = 'SidebarMenuItem'
+
+const SidebarMenuButton = forwardRef<
+  HTMLButtonElement,
+  ComponentProps<'button'> & {
+    asChild?: boolean
+    isActive?: boolean
+    tooltip?: string | ComponentProps<typeof TooltipContent>
+  }
+>(({ asChild = false, isActive = false, tooltip, ...props }, ref) => {
+  const Comp = asChild ? Slot : 'button'
+  const { state } = useSidebar()
+
+  const button = (
+    <Comp
+      ref={ref}
+      data-sidebar="menu-button"
+      data-active={isActive}
+      {...props}
+    />
+  )
+
+  if (!tooltip) {
+    return button
+  }
+
+  if (typeof tooltip === 'string') {
+    tooltip = {
+      children: tooltip,
+    }
+  }
+
+  return (
+    <TooltipRoot>
+      <TooltipTrigger asChild>{button}</TooltipTrigger>
+      <TooltipContent
+        side="right"
+        align="center"
+        hidden={state !== 'collapsed'}
+        {...tooltip}
+      />
+    </TooltipRoot>
+  )
+})
+SidebarMenuButton.displayName = 'SidebarMenuButton'
+
+const SidebarMenuAction = forwardRef<
+  HTMLButtonElement,
+  ComponentProps<'button'> & {
+    asChild?: boolean
+    showOnHover?: boolean
+  }
+>(({ className, asChild = false, showOnHover = false, ...props }, ref) => {
+  const Comp = asChild ? Slot : 'button'
+
+  return <Comp ref={ref} data-sidebar="menu-action" {...props} />
+})
+SidebarMenuAction.displayName = 'SidebarMenuAction'
+
+const SidebarMenuBadge = forwardRef<HTMLDivElement, ComponentProps<'div'>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} data-sidebar="menu-badge" {...props} />
+  ),
+)
+SidebarMenuBadge.displayName = 'SidebarMenuBadge'
+
+const SidebarMenuSub = forwardRef<HTMLUListElement, ComponentProps<'ul'>>(
+  ({ className, ...props }, ref) => (
+    <ul ref={ref} data-sidebar="menu-sub" {...props} />
+  ),
+)
+SidebarMenuSub.displayName = 'SidebarMenuSub'
+
+const SidebarMenuSubItem = forwardRef<HTMLLIElement, ComponentProps<'li'>>(
+  ({ ...props }, ref) => <li ref={ref} {...props} />,
+)
+SidebarMenuSubItem.displayName = 'SidebarMenuSubItem'
+
+const SidebarMenuSubButton = forwardRef<
+  HTMLAnchorElement,
+  ComponentProps<'a'> & {
+    asChild?: boolean
+    size?: 'sm' | 'md'
+    isActive?: boolean
+  }
+>(({ asChild = false, size = 'md', isActive, className, ...props }, ref) => {
+  const Comp = asChild ? Slot : 'a'
+
+  return (
+    <Comp
+      ref={ref}
+      data-sidebar="menu-sub-button"
+      data-size={size}
+      data-active={isActive}
+      {...props}
+    />
+  )
+})
+SidebarMenuSubButton.displayName = 'SidebarMenuSubButton'
+
+export {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuAction,
+  SidebarMenuBadge,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarProvider,
+  SidebarRail,
+  SidebarTrigger,
+  useSidebar,
+}
