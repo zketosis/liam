74fcb2b4892ddd68eaac5b768f60cab2c8569851
MH4GF <h.miyagi.cnw@gmail.com>
2024-12-02 17:39:56 +0900

    feat: refactor ERDRenderer and ERDContent to use DBStructure store directly

--- frontend/packages/erd-core/src/components/ERDRenderer/ERDRenderer.tsx
+++ frontend/packages/erd-core/src/components/ERDRenderer/ERDRenderer.tsx
@@ -1,19 +1,15 @@
 import '@xyflow/react/dist/style.css'
 import { ReactFlowProvider } from '@xyflow/react'
 import type { FC } from 'react'
-import { useDBStructureStore } from '../../stores'
 import { ERDContent } from './ERDContent'
 import styles from './ERDRenderer.module.css'
-import { convertDBStructureToNodes } from './convertDBStructureToNodes'
 
 export const ERDRenderer: FC = () => {
-  const dbStructure = useDBStructureStore()
-  const nodes = convertDBStructureToNodes(dbStructure)
   return (
     <main className={styles.wrapper}>
       <div>sidebar</div>
       <ReactFlowProvider>
-        <ERDContent nodes={nodes} />
+        <ERDContent />
       </ReactFlowProvider>
     </main>
   )
