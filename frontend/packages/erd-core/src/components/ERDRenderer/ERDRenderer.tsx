290691cc88c5f67b9539003ef71d452966692a83
MH4GF <h.miyagi.cnw@gmail.com>
2024-12-02 14:47:14 +0900

    feat: Setup 2 columns layout

--- frontend/packages/erd-core/src/components/ERDRenderer/ERDRenderer.tsx
+++ frontend/packages/erd-core/src/components/ERDRenderer/ERDRenderer.tsx
@@ -3,14 +3,18 @@ import { ReactFlowProvider } from '@xyflow/react'
 import type { FC } from 'react'
 import { useDBStructureStore } from '../../stores'
 import { ERDContent } from './ERDContent'
+import styles from './ERDRenderer.module.css'
 import { convertDBStructureToNodes } from './convertDBStructureToNodes'
 
 export const ERDRenderer: FC = () => {
   const dbStructure = useDBStructureStore()
   const nodes = convertDBStructureToNodes(dbStructure)
   return (
-    <ReactFlowProvider>
-      <ERDContent nodes={nodes} />
-    </ReactFlowProvider>
+    <main className={styles.wrapper}>
+      <div>sidebar</div>
+      <ReactFlowProvider>
+        <ERDContent nodes={nodes} />
+      </ReactFlowProvider>
+    </main>
   )
 }
