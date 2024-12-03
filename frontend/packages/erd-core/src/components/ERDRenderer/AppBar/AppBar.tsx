50d55a1f7d0946b4b8efa77f1886aa124c1578a1
junkisai <jonki.beam@gmail.com>
2024-12-03 14:02:25 +0900

    feat: Add AppBar

---
+++ frontend/packages/erd-core/src/components/ERDRenderer/AppBar/AppBar.tsx
@@ -0,0 +1,48 @@
+import {
+  Button,
+  LiamLogoMark,
+  TooltipContent,
+  TooltipPortal,
+  TooltipProvider,
+  TooltipRoot,
+  TooltipTrigger,
+} from '@liam-hq/ui'
+import type { FC } from 'react'
+import styles from './AppBar.module.css'
+import { ExportButton } from './ExportButton'
+import { GithubButton } from './GithubButton'
+import { HelpButton } from './HelpButton'
+import { ReleaseNoteButton } from './ReleaseNoteButton'
+
+export const AppBar: FC = () => {
+  return (
+    <header className={styles.wrapper}>
+      <TooltipProvider>
+        <TooltipRoot>
+          <TooltipTrigger asChild>
+            <a href="https://liambx.com" target="_blank" rel="noreferrer">
+              <LiamLogoMark className={styles.logo} />
+            </a>
+          </TooltipTrigger>
+          <TooltipPortal>
+            <TooltipContent sideOffset={4}>Go to Liambx.com</TooltipContent>
+          </TooltipPortal>
+        </TooltipRoot>
+      </TooltipProvider>
+
+      <h1 className={styles.title}>ERD Generator</h1>
+
+      <div className={styles.rightSide}>
+        <div className={styles.iconButtonGroup}>
+          <GithubButton />
+          <ReleaseNoteButton />
+          <HelpButton />
+        </div>
+        <ExportButton />
+        <Button variant="solid-primary" size="md">
+          Share
+        </Button>
+      </div>
+    </header>
+  )
+}
