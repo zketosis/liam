83c03d3dc12c266c31b370f53ee108a60a3541bb
junkisai <jonki.beam@gmail.com>
2024-12-03 17:48:11 +0900

    feat: feat: Display Drawer from the right when Entity is clicked

--- frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/TableNode/TableNode.tsx
+++ frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/TableNode/TableNode.tsx
@@ -1,5 +1,12 @@
 import type { Table } from '@liam-hq/db-structure'
-import { KeyRound } from '@liam-hq/ui'
+import {
+  DrawerClose,
+  DrawerContent,
+  DrawerPortal,
+  DrawerRoot,
+  DrawerTrigger,
+  KeyRound,
+} from '@liam-hq/ui'
 import type { Node, NodeProps } from '@xyflow/react'
 import type { FC } from 'react'
 import styles from './TableNode.module.css'
@@ -14,28 +21,40 @@ type Props = NodeProps<TableNodeType>
 
 export const TableNode: FC<Props> = ({ data: { table } }) => {
   return (
-    <div className={styles.wrapper}>
-      <div>{table.name}</div>
-      <ul>
-        {Object.values(table.columns).map((column) => (
-          <li key={column.name}>
-            <span>
-              {column.primary && (
-                <KeyRound
-                  width={16}
-                  height={16}
-                  role="img"
-                  aria-label="Primary Key"
-                />
-              )}
-            </span>
-            <span> </span>
-            <span>{column.name}</span>
-            <span> </span>
-            <span>{column.type}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <DrawerRoot direction="right">
+        <DrawerTrigger>
+          <div className={styles.wrapper}>
+            <div>{table.name}</div>
+            <ul>
+              {Object.values(table.columns).map((column) => (
+                <li key={column.name}>
+                  <span>
+                    {column.primary && (
+                      <KeyRound
+                        width={16}
+                        height={16}
+                        role="img"
+                        aria-label="Primary Key"
+                      />
+                    )}
+                  </span>
+                  <span> </span>
+                  <span>{column.name}</span>
+                  <span> </span>
+                  <span>{column.type}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </DrawerTrigger>
+        <DrawerPortal>
+          <DrawerContent className={styles.content}>
+            <DrawerClose>Close</DrawerClose>
+            <div>TODO: Render TableDetail Component</div>
+          </DrawerContent>
+        </DrawerPortal>
+      </DrawerRoot>
+    </>
   )
 }
