30700977a27d25f4d67052e158ed9c8a5d239be2
ryota.sasazawa <ryota.sasazawa@route06.co.jp>
2024-12-03 12:28:21 +0900

    Refactor schemarb parser to use prism parser

--- frontend/packages/db-structure/src/parser/schemarb/index.ts
+++ frontend/packages/db-structure/src/parser/schemarb/index.ts
@@ -1,8 +1,1 @@
-import type { Processor } from '../types.js'
-import { convertToDBStructure } from './converter.js'
-import { parse } from './parser.js'
-
-export const processor: Processor = (str) => {
-  const parsed = parse(str)
-  return convertToDBStructure(parsed)
-}
+export { processor } from './parser'
