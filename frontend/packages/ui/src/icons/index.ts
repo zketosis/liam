b380dff1d4ed7ff453d824278a1b933b805cefad
junkisai <jonki.beam@gmail.com>
2024-12-05 09:52:06 +0900

    feat: Add XIcon

--- frontend/packages/ui/src/icons/index.ts
+++ frontend/packages/ui/src/icons/index.ts
@@ -4,4 +4,6 @@ export * from './DiamondIcon'
 export * from './DiamondFillIcon'
 export * from './Table2'
 export * from './PanelLeft'
+export * from './XIcon'
+
 export { KeyRound } from 'lucide-react'
