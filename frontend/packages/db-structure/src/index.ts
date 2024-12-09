78615369c8e13500fc791d91b20f8415a8a062fd
MH4GF <h.miyagi.cnw@gmail.com>
2024-12-09 19:03:50 +0900

    feat: Implement extractDBStructureForTable function to retrieve related tables and relationships

--- frontend/packages/db-structure/src/index.ts
+++ frontend/packages/db-structure/src/index.ts
@@ -1,9 +1,14 @@
 export {
   type DBStructure,
   type Table,
+  type Tables,
   type Columns,
   type Column,
   type Index,
   type Indices,
+  type Relationships,
   dbStructureSchema,
+  aTable,
+  aColumn,
+  aRelationship,
 } from './schema/index.js'
