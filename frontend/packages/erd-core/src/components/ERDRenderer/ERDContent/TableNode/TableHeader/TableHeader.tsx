0399ad15842b23b6f731754192e1b2d9b06605c0
junkisai <jonki.beam@gmail.com>
2024-12-12 12:09:46 +0900

    feat: add show mode support for TableHeader and TableNode components

--- frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/TableNode/TableHeader/TableHeader.tsx
+++ frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/TableNode/TableHeader/TableHeader.tsx
@@ -1,4 +1,6 @@
+import { useDBStructureStore, useUserEditingStore } from '@/stores'
 import { Table2 } from '@liam-hq/ui'
+import { Handle, Position } from '@xyflow/react'
 import type { FC } from 'react'
 import styles from './TableHeader.module.css'
 
@@ -7,10 +9,30 @@ type Props = {
 }
 
 export const TableHeader: FC<Props> = ({ name }) => {
+  const { showMode } = useUserEditingStore()
+  const { relationships } = useDBStructureStore()
+
+  const isTarget = Object.values(relationships).some(
+    (relationship) => relationship.foreignTableName === name,
+  )
+  const isSource = Object.values(relationships).some(
+    (relationship) => relationship.primaryTableName === name,
+  )
+
   return (
     <div className={styles.wrapper}>
       <Table2 width={16} />
       <span className={styles.name}>{name}</span>
+      {showMode === 'TABLE_NAME' && (
+        <>
+          {isTarget && (
+            <Handle id={name} type="target" position={Position.Left} />
+          )}
+          {isSource && (
+            <Handle id={name} type="source" position={Position.Right} />
+          )}
+        </>
+      )}
     </div>
   )
 }
