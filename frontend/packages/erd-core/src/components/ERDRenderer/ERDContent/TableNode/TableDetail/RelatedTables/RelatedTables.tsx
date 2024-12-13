ed09420bd1dee512d61bec570bfede727b37fc55
MH4GF <h.miyagi.cnw@gmail.com>
2024-12-13 20:22:43 +0900

    feat: Implement click handler for IconButton to update active table and manage node visibility

--- frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/TableNode/TableDetail/RelatedTables/RelatedTables.tsx
+++ frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/TableNode/TableDetail/RelatedTables/RelatedTables.tsx
@@ -1,9 +1,9 @@
 import { convertDBStructureToNodes } from '@/components/ERDRenderer/convertDBStructureToNodes'
-import { useDBStructureStore } from '@/stores'
+import { updateActiveTableName, useDBStructureStore } from '@/stores'
 import type { Table } from '@liam-hq/db-structure'
 import { GotoIcon, IconButton } from '@liam-hq/ui'
-import { ReactFlowProvider } from '@xyflow/react'
-import type { FC } from 'react'
+import { ReactFlowProvider, useReactFlow } from '@xyflow/react'
+import { type FC, useCallback } from 'react'
 import { ERDContent } from '../../../ERDContent'
 import styles from './RelatedTables.module.css'
 import { extractDBStructureForTable } from './extractDBStructureForTable'
@@ -19,12 +19,27 @@ export const RelatedTables: FC<Props> = ({ table }) => {
     dbStructure: extractedDBStructure,
     showMode: 'TABLE_NAME',
   })
+  const { setNodes } = useReactFlow()
+  const handleClick = useCallback(() => {
+    const visibleNodeIds: string[] = nodes.map((node) => node.id)
+    setNodes((mainPaneNodes) => {
+      return mainPaneNodes.map((node) => ({
+        ...node,
+        hidden: !visibleNodeIds.includes(node.id),
+      }))
+    })
+    updateActiveTableName(undefined)
+  }, [nodes, setNodes])
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.header}>
         <h2 className={styles.heading}>Related tables</h2>
-        <IconButton icon={<GotoIcon />} tooltipContent="Go to Related tables" />
+        <IconButton
+          icon={<GotoIcon />}
+          tooltipContent="Go to Related tables"
+          onClick={handleClick}
+        />
       </div>
       <div className={styles.contentWrapper}>
         <ReactFlowProvider>
