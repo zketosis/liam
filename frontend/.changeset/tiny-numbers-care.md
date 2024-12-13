12360ab327552c85e26920168c5b0d605d3d77f9
ryota.sasazawa <ryota.sasazawa@route06.co.jp>
2024-12-13 17:47:11 +0900

    Reflect active table into url

---
+++ frontend/.changeset/tiny-numbers-care.md
@@ -0,0 +1,6 @@
+---
+"@liam-hq/erd-core": patch
+"@liam-hq/cli": patch
+---
+
+Reflect active table into url
--- frontend/packages/erd-core/src/stores/userEditing/store.ts
+++ frontend/packages/erd-core/src/stores/userEditing/store.ts
@@ -1,5 +1,5 @@
 import type { ShowMode } from '@/schemas/showMode'
-import { proxy } from 'valtio'
+import { proxy, subscribe } from 'valtio'
 
 type UserEditingStore = {
   active: {
@@ -13,4 +13,17 @@ export const userEditingStore = proxy<UserEditingStore>({
     tableName: undefined,
   },
   showMode: 'ALL_FIELDS',
+})
+
+subscribe(userEditingStore.active, () => {
+  const newTableName = userEditingStore.active.tableName
+  const url = new URL(window.location.href)
+
+  if (newTableName) {
+    url.searchParams.set('active', newTableName)
+  } else {
+    url.searchParams.delete('active')
+  }
+
+  window.history.pushState({}, '', url)
 })
