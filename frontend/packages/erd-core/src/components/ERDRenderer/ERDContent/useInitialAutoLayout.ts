9c44a6a274371fb3237e241a2b269f50dc7411f0
MH4GF <h.miyagi.cnw@gmail.com>
2024-12-16 18:12:25 +0900

    fix: Fixed an issue where the correct table was not focused when sharing URLs in TableDetail

--- frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/useInitialAutoLayout.ts
+++ frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/useInitialAutoLayout.ts
@@ -1,7 +1,17 @@
+import type { QueryParam } from '@/schemas/queryParam'
+import { updateActiveTableName } from '@/stores'
 import { useNodesInitialized } from '@xyflow/react'
 import { useEffect } from 'react'
 import { useERDContentContext } from './ERDContentContext'
 import { useAutoLayout } from './useAutoLayout'
+
+const getActiveTableNameFromUrl = (): string | undefined => {
+  const urlParams = new URLSearchParams(window.location.search)
+  const activeQueryParam: QueryParam = 'active'
+  const tableName = urlParams.get(activeQueryParam)
+
+  return tableName || undefined
+}
 
 export const useInitialAutoLayout = () => {
   const nodesInitialized = useNodesInitialized()
@@ -15,8 +25,14 @@ export const useInitialAutoLayout = () => {
       return
     }
 
+    const tableNameFromUrl = getActiveTableNameFromUrl()
+    updateActiveTableName(tableNameFromUrl)
+    const fitViewOptions = tableNameFromUrl
+      ? { maxZoom: 1, duration: 300, nodes: [{ id: tableNameFromUrl }] }
+      : undefined
+
     if (nodesInitialized) {
-      handleLayout()
+      handleLayout(fitViewOptions)
     }
   }, [nodesInitialized, initializeComplete, handleLayout])
 }
