a85acb3d3ec673b77a87090f38c7ef72f62e84d3
MH4GF <h.miyagi.cnw@gmail.com>
2024-12-16 18:09:25 +0900

    fix: Fixed an issue where URL sharing in TableDetail sometimes did not work correctly

--- frontend/packages/erd-core/src/stores/userEditing/hooks.ts
+++ frontend/packages/erd-core/src/stores/userEditing/hooks.ts
@@ -2,3 +2,5 @@ import { useSnapshot } from 'valtio'
 import { userEditingStore } from './store'
 
 export const useUserEditingStore = () => useSnapshot(userEditingStore)
+export const useUserEditingActiveStore = () =>
+  useSnapshot(userEditingStore.active)
