cd43bb39211ede893c0b2eb13eefc652af6b2795
MH4GF <h.miyagi.cnw@gmail.com>
2024-12-16 14:52:06 +0900

    feat: Enhance layout handling by filtering visible nodes and edges in useAutoLayout

--- frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/useAutoLayout/useAutoLayout.ts
+++ frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/useAutoLayout/useAutoLayout.ts
@@ -1,4 +1,5 @@
 import { useReactFlow } from '@xyflow/react'
+import type { Node } from '@xyflow/react'
 import { useCallback } from 'react'
 import { getElkLayout } from './getElkLayout'
 
@@ -8,13 +9,21 @@ export const useAutoLayout = () => {
   const handleLayout = useCallback(async () => {
     const nodes = getNodes()
     const edges = getEdges()
+    const visibleNodes: Node[] = nodes.filter((node) => !node.hidden)
+    const hiddenNodes: Node[] = nodes.filter((node) => node.hidden)
 
-    const newNodes = await getElkLayout({
-      nodes,
-      edges,
+    // NOTE: Only include edges where both the source and target are in the nodes
+    const nodeMap = new Map(visibleNodes.map((node) => [node.id, node]))
+    const visibleEdges = edges.filter((edge) => {
+      return nodeMap.get(edge.source) && nodeMap.get(edge.target)
     })
 
-    setNodes(newNodes)
+    const newNodes = await getElkLayout({
+      nodes: visibleNodes,
+      edges: visibleEdges,
+    })
+
+    setNodes([...hiddenNodes, ...newNodes])
     setTimeout(() => fitView(), 0)
   }, [getNodes, setNodes, getEdges, fitView])
 
