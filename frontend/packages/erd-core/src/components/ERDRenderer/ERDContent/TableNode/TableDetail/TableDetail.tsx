690b1594bb2e7a45150dca7eb4a1747c9b9975c2
hoshinotsuyoshi <tsuyoshi.hoshino@route06.co.jp>
2024-12-19 14:33:38 +0900

    Ensure `related tables` reload when any table node becomes active

--- frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/TableNode/TableDetail/TableDetail.tsx
+++ frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/TableNode/TableDetail/TableDetail.tsx
@@ -39,7 +39,7 @@ export const TableDetail: FC<Props> = ({ table }) => {
         <Columns columns={table.columns} />
         <Indices indices={table.indices} />
         <Unique columns={table.columns} />
-        <RelatedTables table={table} />
+        <RelatedTables key={table.name} table={table} />
       </div>
     </section>
   )
