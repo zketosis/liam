7cc97e7de8e0960f655363d3473a38d85906bbd1
FunamaYukina <yukina.funama@route06.co.jp>
2024-12-24 19:46:32 +0900

    update assetPrefix to use environment variable

--- frontend/apps/docs/next.config.mjs
+++ frontend/apps/docs/next.config.mjs
@@ -2,18 +2,10 @@ import { createMDX } from 'fumadocs-mdx/next'
 
 const withMDX = createMDX()
 
-function isProductionEnvironment() {
-  if (typeof window !== 'undefined') {
-    return window.location.hostname === 'liam-docs-sigma.vercel.app'
-  }
-  return false
-}
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  assetPrefix: isProductionEnvironment()
-    ? 'https://liam-docs-sigma.vercel.app'
-    : '',
+  assetPrefix: process.env.ASSET_PREFIX || '',
 }
 
 export default withMDX(config)
