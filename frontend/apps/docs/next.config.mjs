f222e43f0270090de31757b9599b4579f4ee0629
FunamaYukina <yukina.funama@route06.co.jp>
2024-12-24 18:43:17 +0900

    Set the assetPrefix to be accessible from liambx

--- frontend/apps/docs/next.config.mjs
+++ frontend/apps/docs/next.config.mjs
@@ -2,9 +2,18 @@ import { createMDX } from 'fumadocs-mdx/next'
 
 const withMDX = createMDX()
 
+function isProductionEnvironment() {
+  if (typeof window !== 'undefined') {
+    return window.location.hostname === 'liam-docs-sigma.vercel.app'
+  }
+  return false
+}
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  assetPrefix: isProductionEnvironment()
+    ? 'https://liam-docs-sigma.vercel.app'
+    : '',
 }
 
 export default withMDX(config)
