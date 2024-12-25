290ce475503a87acbcb275a66e75b0298a9d3c68
ryota.sasazawa <ryota.sasazawa@route06.co.jp>
2024-12-25 17:06:07 +0900

    Revert "♻️: Add transpilePackages"
    This reverts commit bc4b166c1abd3a155fa90bf052fa5727f499189e.

--- frontend/apps/erd-web/next.config.ts
+++ frontend/apps/erd-web/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-  transpilePackages: ['@liam-hq/db-structure', '@liam-hq/erd-core'],
 }
 
 export default nextConfig
