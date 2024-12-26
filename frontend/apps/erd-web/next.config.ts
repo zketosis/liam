9b890379cf02eb899a382d3d8b0101e07da794e2
ryota.sasazawa <ryota.sasazawa@route06.co.jp>
2024-12-26 15:26:07 +0900

    ✨: Update next config to extract gitHash and releaseDate

--- frontend/apps/erd-web/next.config.ts
+++ frontend/apps/erd-web/next.config.ts
@@ -1,7 +1,15 @@
+// biome-ignore lint/correctness/noNodejsModules: This code is only run in the Node.js environment.
+import { execSync } from 'node:child_process'
 import type { NextConfig } from 'next'
 
+const gitCommitHash = execSync('git rev-parse --short HEAD').toString().trim()
+const releaseDate = new Date().toISOString().split('T')[0]
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_GIT_HASH: gitCommitHash,
+    NEXT_PUBLIC_RELEASE_DATE: releaseDate,
+  },
 }
 
 export default nextConfig
