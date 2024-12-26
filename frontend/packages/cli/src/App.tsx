84a3ee3750c884ee66ff78d19691ea6a93757749
ryota.sasazawa <ryota.sasazawa@route06.co.jp>
2024-12-26 17:22:33 +0900

    🐛 : Fix displayedOn for CLI

--- frontend/packages/cli/src/App.tsx
+++ frontend/packages/cli/src/App.tsx
@@ -32,7 +32,7 @@ const cliVersionData = {
   isReleasedGitHash:
     import.meta.env.VITE_CLI_VERSION_IS_RELEASED_GIT_HASH === '1',
   date: import.meta.env.VITE_CLI_VERSION_DATE,
-  displayedOn: 'web',
+  displayedOn: 'cli',
 }
 const cliVersion = v.parse(cliVersionSchema, cliVersionData)
 
