ff674a05a26bc6ce08aed1bdb821bea4b3d643c5
MH4GF <h.miyagi.cnw@gmail.com>
2025-01-03 16:12:03 +0900

    debug: add sample routing for sentry

---
+++ frontend/apps/erd-web/app/api/sentry-example-api/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server'
+
+export const dynamic = 'force-dynamic'
+
+// A faulty API route to test Sentry's error monitoring
+export function GET() {
+  throw new Error('Sentry Example API Route Error')
+}
