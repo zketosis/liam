35bad400debe3957f06e30181cb629207dfb19ac
ryota.sasazawa <ryota.sasazawa@route06.co.jp>
2025-01-21 16:05:06 +0900

    🔧 Set Google Tag Manager

--- frontend/apps/erd-web/app/layout.tsx
+++ frontend/apps/erd-web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import type React from 'react'
 import './globals.css'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -35,6 +36,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {process.env.NEXT_PUBLIC_ENV_NAME === 'development' && (
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID ?? ''} />
+      )}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
