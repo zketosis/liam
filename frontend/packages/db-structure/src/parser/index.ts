727ae2cca693e746d97c64cd35072a625072a16b
ryota.sasazawa <ryota.sasazawa@route06.co.jp>
2025-01-27 15:43:14 +0900

    ✨ Add tbls format support to database schema parser

--- frontend/packages/db-structure/src/parser/index.ts
+++ frontend/packages/db-structure/src/parser/index.ts
@@ -2,6 +2,7 @@ import { processor as prismaProcessor } from './prisma/index.js'
 import { processor as schemarbProcessor } from './schemarb/index.js'
 import { processor as postgresqlProcessor } from './sql/index.js'
 import type { SupportedFormat } from './supportedFormat/index.js'
+import { processor as tblsProcessor } from './tbls/index.js'
 import type { ProcessResult } from './types.js'
 
 export { ProcessError } from './errors.js'
@@ -23,5 +24,7 @@ export const parse = (
       return postgresqlProcessor(str)
     case 'prisma':
       return prismaProcessor(str)
+    case 'tbls':
+      return tblsProcessor(str)
   }
 }
