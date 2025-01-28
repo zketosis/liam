7653aaab3db4d5deaee6a2929e635f9f992f8bf9
Ryota Sasazawa <65695538+sasamuku@users.noreply.github.com>
2025-01-28 20:29:43 +0900

    Revert "💄 Add styling for mobile widths"

--- frontend/packages/erd-core/src/components/ERDRenderer/AppBar/AppBar.tsx
+++ frontend/packages/erd-core/src/components/ERDRenderer/AppBar/AppBar.tsx
@@ -11,29 +11,23 @@ import styles from './AppBar.module.css'
 import { CopyLinkButton } from './CopyLinkButton'
 import { GithubButton } from './GithubButton'
 import { HelpButton } from './HelpButton'
-import { MenuButton } from './MenuButton'
 import { ReleaseNoteButton } from './ReleaseNoteButton'
 
 export const AppBar: FC = () => {
   return (
     <header className={styles.wrapper}>
-      <div className={styles.menuButtonWrapper}>
-        <MenuButton />
-      </div>
-      <div className={styles.logoWrapper}>
-        <TooltipProvider>
-          <TooltipRoot>
-            <TooltipTrigger asChild>
-              <a href="https://liambx.com" target="_blank" rel="noreferrer">
-                <LiamLogoMark className={styles.logo} />
-              </a>
-            </TooltipTrigger>
-            <TooltipPortal>
-              <TooltipContent sideOffset={4}>Go to Home page</TooltipContent>
-            </TooltipPortal>
-          </TooltipRoot>
-        </TooltipProvider>
-      </div>
+      <TooltipProvider>
+        <TooltipRoot>
+          <TooltipTrigger asChild>
+            <a href="https://liambx.com" target="_blank" rel="noreferrer">
+              <LiamLogoMark className={styles.logo} />
+            </a>
+          </TooltipTrigger>
+          <TooltipPortal>
+            <TooltipContent sideOffset={4}>Go to Home page</TooltipContent>
+          </TooltipPortal>
+        </TooltipRoot>
+      </TooltipProvider>
 
       <h1 className={styles.title}>Liam ERD</h1>
 
