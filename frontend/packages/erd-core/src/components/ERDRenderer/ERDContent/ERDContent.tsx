bedba3d9cf302c8eb1520c87b21ad449c48d0cf2
Prakhar Dewan <33227141+prakha@users.noreply.github.com>
2025-02-21 23:50:11 +0530

    feat: Add dark mode support to ERDContent

--- frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/ERDContent.tsx
+++ frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/ERDContent.tsx
@@ -139,6 +139,7 @@ export const ERDContentInner: FC<Props> = ({
     <div className={styles.wrapper} data-loading={loading}>
       {loading && <Spinner className={styles.loading} />}
       <ReactFlow
+        colorMode='dark'
         nodes={nodes}
         edges={edges}
         nodeTypes={nodeTypes}
