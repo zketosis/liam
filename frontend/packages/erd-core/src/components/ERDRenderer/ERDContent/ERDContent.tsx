3d5458391b13374d61e34dca42373e233438433e
Prakhar Dewan <33227141+prakha@users.noreply.github.com>
2025-02-22 09:45:05 +0530

    fix: resolved lint errors

--- frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/ERDContent.tsx
+++ frontend/packages/erd-core/src/components/ERDRenderer/ERDContent/ERDContent.tsx
@@ -139,7 +139,7 @@ export const ERDContentInner: FC<Props> = ({
     <div className={styles.wrapper} data-loading={loading}>
       {loading && <Spinner className={styles.loading} />}
       <ReactFlow
-        colorMode='dark'
+        colorMode="dark"
         nodes={nodes}
         edges={edges}
         nodeTypes={nodeTypes}
