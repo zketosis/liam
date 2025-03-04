d9102495eb299dc58a813bf35bd59a904636b943
IkedaNoritaka <50833174+NoritakaIkeda@users.noreply.github.com>
2025-03-04 18:04:06 +0900

    ✨ Add hover effect to sidebar

--- frontend/packages/ui/src/components/Resizable/Resizable.tsx
+++ frontend/packages/ui/src/components/Resizable/Resizable.tsx
@@ -4,16 +4,14 @@ import clsx from 'clsx'
 import { GripVertical } from 'lucide-react'
 import type { ComponentProps } from 'react'
 import * as ResizablePrimitive from 'react-resizable-panels'
+import styles from './Resizable.module.css'
 
 const ResizablePanelGroup = ({
   className,
   ...props
 }: ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
   <ResizablePrimitive.PanelGroup
-    className={clsx(
-      'flex h-full w-full data-[panel-group-direction=vertical]:flex-col',
-      className,
-    )}
+    className={clsx(styles.panelGroup, className)}
     {...props}
   />
 )
@@ -30,14 +28,11 @@ const ResizableHandle = ({
   withHandle?: boolean
 }) => (
   <ResizablePrimitive.PanelResizeHandle
-    className={clsx(
-      'relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90',
-      className,
-    )}
+    className={clsx(styles.handle, className)}
     {...props}
   >
     {withHandle && (
-      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
+      <div className={styles.handleIcon}>
         <GripVertical className="h-2.5 w-2.5" />
       </div>
     )}
