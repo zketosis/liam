35ed2f5eaaa8210ab1602ce188ad7889fa61b7f4
IkedaNoritaka <50833174+NoritakaIkeda@users.noreply.github.com>
2025-03-11 17:06:21 +0900

    🔧 Refactor file structure and update imports in the ProjectsPage component

---
+++ frontend/apps/app/app/(app)/app/projects/page.tsx
@@ -0,0 +1,14 @@
+import { ProjectsPage } from '@/features/projects/pages'
+import { migrationFlag } from '@/libs'
+import { notFound } from 'next/navigation'
+import React from 'react'
+
+export default async function Page() {
+  const migrationEnabled = await migrationFlag()
+
+  if (!migrationEnabled) {
+    notFound()
+  }
+
+  return <ProjectsPage />
+}
