721f94cff518d6be047f897f537942e30944eb35
Prakhar Dewan <33227141+prakha@users.noreply.github.com>
2025-03-30 18:32:24 +0530

    wrapped the component inside tableNode with tooltipProvider

--- frontend/packages/erd-core/src/features/erd/components/ERDContent/components/TableNode/TableNode.tsx
+++ frontend/packages/erd-core/src/features/erd/components/ERDContent/components/TableNode/TableNode.tsx
@@ -1,5 +1,12 @@
 import type { TableNodeType } from '@/features/erd/types'
 import { useUserEditingStore } from '@/stores'
+import {
+  TooltipContent,
+  TooltipPortal,
+  TooltipProvider,
+  TooltipRoot,
+  TooltipTrigger,
+} from '@liam-hq/ui'
 import type { NodeProps } from '@xyflow/react'
 import clsx from 'clsx'
 import type { FC } from 'react'
@@ -12,24 +19,37 @@ type Props = NodeProps<TableNodeType>
 export const TableNode: FC<Props> = ({ data }) => {
   const { showMode: _showMode } = useUserEditingStore()
   const showMode = data.showMode ?? _showMode
+  const name = data?.table?.name
 
   return (
-    <div
-      className={clsx(
-        styles.wrapper,
-        data.isHighlighted && styles.wrapperHighlighted,
-        data.isActiveHighlighted && styles.wrapperActive,
-      )}
-      data-erd={
-        (data.isHighlighted || data.isActiveHighlighted) &&
-        'table-node-highlighted'
-      }
-    >
-      <TableHeader data={data} />
-      {showMode === 'ALL_FIELDS' && <TableColumnList data={data} />}
-      {showMode === 'KEY_ONLY' && (
-        <TableColumnList data={data} filter="KEY_ONLY" />
-      )}
-    </div>
+    <TooltipProvider>
+      <TooltipRoot>
+        <TooltipTrigger asChild>
+          <div
+            className={clsx(
+              styles.wrapper,
+              data.isHighlighted && styles.wrapperHighlighted,
+              data.isActiveHighlighted && styles.wrapperActive,
+            )}
+            data-erd={
+              (data.isHighlighted || data.isActiveHighlighted) &&
+              'table-node-highlighted'
+            }
+          >
+            <TableHeader data={data} />
+            {showMode === 'ALL_FIELDS' && <TableColumnList data={data} />}
+            {showMode === 'KEY_ONLY' && (
+              <TableColumnList data={data} filter="KEY_ONLY" />
+            )}
+          </div>
+        </TooltipTrigger>
+
+        <TooltipPortal>
+          <TooltipContent side={'top'} sideOffset={4}>
+            {name}
+          </TooltipContent>
+        </TooltipPortal>
+      </TooltipRoot>
+    </TooltipProvider>
   )
 }
