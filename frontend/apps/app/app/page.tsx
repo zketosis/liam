583b000950ef50852f5f8f67677ecccace1444f5
hoshinotsuyoshi <tsuyoshi.hoshino@route06.co.jp>
2025-03-31 14:44:27 +0900

    Update frontend/apps/app app page redirect

--- frontend/apps/app/app/page.tsx
+++ frontend/apps/app/app/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from 'next/navigation'
 
 export default function Page() {
-  redirect('/erd/p/github.com/mastodon/mastodon/blob/main/db/schema.rb')
+  redirect(
+    '/erd/p/github.com/mastodon/mastodon/blob/1bc28709ccde4106ab7d654ad5888a14c6bb1724/db/schema.rb',
+  )
 }
