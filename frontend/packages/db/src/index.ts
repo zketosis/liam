f26b67223b2d5b25e5980621eaa4eaf8988f1849
MH4GF <h.miyagi.cnw@gmail.com>
2025-04-01 20:33:54 +0900

    feat: Setup Supabase client integration for jobs package

--- frontend/packages/db/src/index.ts
+++ frontend/packages/db/src/index.ts
@@ -3,10 +3,18 @@ import {
   createBrowserClient as _createBrowserClient,
   createServerClient as _createServerClient,
 } from '@supabase/ssr'
+import { createClient as _createClient } from '@supabase/supabase-js'
 import type { Database } from '../supabase/database.types'
 
 export const prisma = new PrismaClient()
 
 export type { EmailOtpType } from '@supabase/supabase-js'
+
+// for Server Components
 export const createServerClient = _createServerClient<Database>
+
+// for Client Components
 export const createBrowserClient = _createBrowserClient<Database>
+
+// for Jobs
+export const createClient = _createClient<Database>
