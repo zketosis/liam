928857b79d39a7cb7cb149cf6a9c365d4e2e4af3
MH4GF <h.miyagi.cnw@gmail.com>
2025-04-03 17:14:06 +0900

    ♻︎ Complete migration from Prisma to Supabase for database migrations, removing old migration scripts and updating package.json with new Supabase migration commands. Documented the new migration workflow in the project documentation.

--- frontend/packages/db/prisma/migrations/20250401081003_delete_overall_score_from_review_issue/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `overallScore` on the `ReviewIssue` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "ReviewIssue" DROP COLUMN "overallScore";
