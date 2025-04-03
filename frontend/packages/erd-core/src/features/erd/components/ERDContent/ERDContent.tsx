4b6947eba979aa3a4a886d59fe9cc11102b3d896
Prakhar Dewan <33227141+prakha@users.noreply.github.com>
2025-04-03 05:40:45 +0530

    modified the hook so that when pop state is active it could highlight the active nodes and edges from the url

--- frontend/packages/erd-core/src/features/erd/components/ERDContent/ERDContent.tsx
+++ frontend/packages/erd-core/src/features/erd/components/ERDContent/ERDContent.tsx
@@ -70,7 +70,7 @@ export const ERDContentInner: FC<Props> = ({
     nodes,
     displayArea,
   })
-  usePopStateListener()
+  usePopStateListener({ nodes, displayArea })
 
   const { version } = useVersion()
   const isTouchDevice = useIsTouchDevice()
--- frontend/packages/erd-core/src/features/erd/components/ERDContent/hooks/usePopStateListener.ts
+++ frontend/packages/erd-core/src/features/erd/components/ERDContent/hooks/usePopStateListener.ts
@@ -1,3 +1,6 @@
+import type { DisplayArea } from '@/features/erd/types'
+import { computeAutoLayout, highlightNodesAndEdges } from '@/features/erd/utils'
+import { useCustomReactflow } from '@/features/reactflow/hooks'
 import {
   replaceHiddenNodeIds,
   updateActiveTableName,
@@ -9,35 +12,69 @@ import {
   getHiddenNodeIdsFromUrl,
   getShowModeFromUrl,
 } from '@/utils'
-import { useEffect } from 'react'
+import type { Node } from '@xyflow/react'
+import { useCallback, useEffect } from 'react'
+import { hasNonRelatedChildNodes, updateNodesHiddenState } from '../utils'
 
-export const usePopStateListener = () => {
-  useEffect(() => {
-    const handlePopState = async () => {
-      const tableName = getActiveTableNameFromUrl()
-      const showMode = getShowModeFromUrl()
-      const hiddenNodeIds = await getHiddenNodeIdsFromUrl()
+type Params = {
+  nodes: Node[]
+  displayArea: DisplayArea
+}
 
-      updateIsPopstateInProgress(true)
+export const usePopStateListener = ({ nodes, displayArea }: Params) => {
+  const { getEdges, setNodes, setEdges, fitView } = useCustomReactflow()
 
-      await new Promise<void>((resolve) => {
+  const handlePopState = useCallback(async () => {
+    updateIsPopstateInProgress(true)
+
+    const [tableName, showMode, hiddenNodeIds] = await Promise.all([
+      getActiveTableNameFromUrl(),
+      getShowModeFromUrl(),
+      getHiddenNodeIdsFromUrl(),
+    ])
+
+    const updatedNodes = updateNodesHiddenState({
+      nodes,
+      hiddenNodeIds,
+      shouldHideGroupNodeId: !hasNonRelatedChildNodes(nodes),
+    })
+
+    await Promise.all([
+      new Promise<void>((resolve) => {
         updateActiveTableName(tableName ?? undefined)
         updateShowMode(showMode)
         setTimeout(resolve, 1)
-      })
-
-      await new Promise<void>((resolve) => {
+      }),
+      new Promise<void>((resolve) => {
         replaceHiddenNodeIds(hiddenNodeIds)
         setTimeout(resolve, 1)
+      }),
+    ])
+
+    const { nodes: highlightedNodes, edges: highlightedEdges } =
+      highlightNodesAndEdges(updatedNodes, getEdges(), {
+        activeTableName: tableName,
       })
 
-      updateIsPopstateInProgress(false)
-    }
+    const { nodes: layoutedNodes, edges: layoutedEdges } =
+      await computeAutoLayout(highlightedNodes, highlightedEdges)
 
+    setNodes(layoutedNodes)
+    setEdges(layoutedEdges)
+
+    const fitViewOptions =
+      displayArea === 'main' && tableName
+        ? { maxZoom: 1, duration: 300, nodes: [{ id: tableName }] }
+        : { duration: 0 }
+    await fitView(fitViewOptions)
+
+    updateIsPopstateInProgress(false)
+  }, [nodes, displayArea, getEdges, setNodes, setEdges, fitView])
+  useEffect(() => {
     window.addEventListener('popstate', handlePopState)
 
     return () => {
       window.removeEventListener('popstate', handlePopState)
     }
-  }, [])
+  }, [handlePopState])
 }
