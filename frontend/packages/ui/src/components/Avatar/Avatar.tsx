90e3df5cb6aebcaa0550ec759b47608b984049f7
Ayumi Kumano <kumayuzuki@gmail.com>
2025-04-16 18:06:49 +0900

    🔧 Separate JackIcon for Avatar

--- frontend/packages/ui/src/components/Avatar/Avatar.tsx
+++ frontend/packages/ui/src/components/Avatar/Avatar.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import type { ComponentProps } from 'react'
+import { match } from 'ts-pattern'
 import styles from './Avatar.module.css'
+import { UserAvatarIcon } from './UserAvatarIcon'
 
 type AvatarSize = 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl'
 type AvatarUser =
@@ -76,20 +78,7 @@ export const Avatar = ({
         type="button"
         {...props}
       >
-        <svg
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className={styles.jackIcon}
-        >
-          <title>User Avatar</title>
-          <path
-            d="M8 0C3.6 0 0 3.6 0 8C0 12.4 3.6 16 8 16C12.4 16 16 12.4 16 8C16 3.6 12.4 0 8 0ZM8 2.4C9.32 2.4 10.4 3.48 10.4 4.8C10.4 6.12 9.32 7.2 8 7.2C6.68 7.2 5.6 6.12 5.6 4.8C5.6 3.48 6.68 2.4 8 2.4ZM8 13.76C6 13.76 4.23 12.76 3.2 11.28C3.23 9.64 6.4 8.72 8 8.72C9.59 8.72 12.77 9.64 12.8 11.28C11.77 12.76 10 13.76 8 13.76Z"
-            fill="var(--global-background)"
-          />
-        </svg>
+        <UserAvatarIcon />
       </button>
     )
   }
