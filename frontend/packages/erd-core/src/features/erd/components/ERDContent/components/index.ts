3fccb50815389ee8e56b60ab7cbdbf339cfa74a8
junkisai <jonki.beam@gmail.com>
2025-04-16 15:58:01 +0900

    ✨ add TableGroupBoundingBox component with styles and export it

--- frontend/packages/erd-core/src/features/erd/components/ERDContent/components/index.ts
+++ frontend/packages/erd-core/src/features/erd/components/ERDContent/components/index.ts
@@ -1,5 +1,6 @@
 export * from './NonRelatedTableGroupNode'
 export * from './RelationshipEdge'
 export * from './Spinner'
+export * from './TableGroupBoundingBox'
 export * from './TableNode'
 export * from './TableGroupNode'
