a46120af71625dee9e928af3b184632579f188b1
FunamaYukina <yukina.funama@route06.co.jp>
2025-04-18 17:23:48 +0900

    🔧 refactor code

--- frontend/apps/app/features/projects/pages/ProjectsPage/ProjectsPage.tsx
+++ frontend/apps/app/features/projects/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,7 +1,12 @@
-import { createClient } from '@/libs/db/server'
-import { redirect } from 'next/navigation'
+import { urlgen } from '@/utils/routes'
+import { EmptyProjectsState } from '../../components/EmptyProjectsState'
 import styles from './ProjectsPage.module.css'
 import { ProjectsWithCommitData } from './ProjectsWithCommitData'
+import {
+  getCurrentOrganization,
+  getUserOrganizations,
+} from './getCurrentOrganization'
+import { getProjects } from './getProjects'
 
 export const runtime = 'edge'
 
@@ -10,51 +15,33 @@ export async function ProjectsPage({
 }: {
   organizationId?: string
 }) {
-  const supabase = await createClient()
-  let baseQuery = supabase.from('projects').select(
-    `
-      *,
-      project_repository_mappings (
-        *,
-        repository:repositories(*)
-      )
-    `,
-  )
-
-  if (organizationId) {
-    baseQuery = baseQuery.eq('organization_id', organizationId)
-  }
-
-  const { data: projects } = await baseQuery
+  const currentOrganization = organizationId
+    ? await getCurrentOrganization(organizationId)
+    : await getCurrentOrganization()
+  await getUserOrganizations() // Fetch for future use
+  const projects = await getProjects(currentOrganization?.id)
 
   return (
     <div className={styles.container}>
       <div className={styles.contentContainer}>
         <h1 className={styles.heading}>Projects</h1>
-        <ProjectsWithCommitData
-          projects={projects}
-          organizationId={organizationId}
-        />
+        {projects === null || projects.length === 0 ? (
+          <EmptyProjectsState
+            createProjectHref={
+              currentOrganization
+                ? urlgen('organizations/[organizationId]/projects/new', {
+                    organizationId: currentOrganization.id,
+                  })
+                : urlgen('organizations/new')
+            }
+          />
+        ) : (
+          <ProjectsWithCommitData
+            projects={projects}
+            organizationId={organizationId}
+          />
+        )}
       </div>
     </div>
   )
-}
-
-export default async function ProjectsPageRoute({
-  params,
-}: {
-  params: { organizationId?: string }
-}) {
-  const organizationId = params.organizationId
-    ? params.organizationId
-    : undefined
-
-  const supabase = await createClient()
-  const { data } = await supabase.auth.getSession()
-
-  if (data.session === null) {
-    return redirect('/login')
-  }
-
-  return <ProjectsPage organizationId={organizationId} />
 }
