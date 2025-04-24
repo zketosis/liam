f04ed45a296d915e775f37c1e96c31c5defc43ae
IkedaNoritaka <50833174+NoritakaIkeda@users.noreply.github.com>
2025-04-24 19:38:38 +0900

    Revert "feat: add organization_id to overall_review_knowledge_suggestion_mappings"
    This reverts commit 9ded7ddf71f8ea2c1ba5dcda5709cfafba8b4827.

--- frontend/packages/db/src/types/supabase-overrides/index.ts
+++ frontend/packages/db/src/types/supabase-overrides/index.ts
@@ -1,9 +1,8 @@
 import type { MergeDeep } from 'type-fest'
 import type { Database as DatabaseGenerated } from '../../../supabase/database.types'
 import type { KnowledgeSuggestionsOverride } from './knowledge_suggestions'
-import type { OverallReviewKnowledgeSuggestionMappingsOverride } from './overall_review_knowledge_suggestion_mappings'
 
 export type AppDatabaseOverrides = MergeDeep<
   DatabaseGenerated,
-  KnowledgeSuggestionsOverride & OverallReviewKnowledgeSuggestionMappingsOverride
+  KnowledgeSuggestionsOverride
 >
